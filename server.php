0282b25d09ff13ff6749c63375804c68bb10fdd4
Psalmuel69 <54561044+Psalmuel69@users.noreply.github.com>
2019-09-20 17:31:19 +0100

    Update Server.php

--- server.php
+++ server.php
@@ -50,7 +50,7 @@ if (isset($_POST['reg_user'])) {
   			  VALUES('$username', '$email', '$password')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "You are now logged in";
+  	$_SESSION['success'] = "Welcome to Team Chernobyl";
   	header('location: index.php');
   }
 }
@@ -75,7 +75,7 @@ if (isset($_POST['login_user'])) {
         $results = mysqli_query($db, $query);
         if (mysqli_num_rows($results) == 1) {
           $_SESSION['username'] = $username;
-          $_SESSION['success'] = "You are now logged in";
+          $_SESSION['success'] = "Welcome to Team Chernobyl";
           header('location: index.php');
         }else {
             array_push($errors, "Wrong username/password combination");
